db0ae473eba14df3ebc97beb57a86e1217e5d669
carlostmgps <61446775+carlostmgps@users.noreply.github.com>
2023-05-12 19:53:53 +0800

    Update main.js

--- testthreejs/main.js
+++ testthreejs/main.js
@@ -8,7 +8,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshPhongMaterial( { color: 0xff00, side: THREE.DoubleSide } );
 const cube = new THREE.Mesh( geometry, material );
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
 scene.add( directionalLight );
