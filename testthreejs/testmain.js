ea7562a28e61396dbb77cf66509b3f279c4f5563
carlostmgps <61446775+carlostmgps@users.noreply.github.com>
2023-05-20 09:45:42 +0800

    Create testmain.js

---
+++ testthreejs/testmain.js
@@ -0,0 +1,58 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
+const material = new THREE.MeshPhongMaterial( { color: 0x00ff00} );
+
+
+const loader = new THREE.TextureLoader();
+const cubeMaterials = [
+    new THREE.MeshPhongMaterial({ map: loader.load('images/side.png'), transparent: false, side: THREE.DoubleSide }), //right side
+    new THREE.MeshPhongMaterial({ map: loader.load('images/side.png'), transparent: false, side: THREE.DoubleSide }), //left side
+    new THREE.MeshPhongMaterial({ map: loader.load('images/grass.png'), transparent: false, side: THREE.DoubleSide }), //top side
+    new THREE.MeshPhongMaterial({ map: loader.load('images/full.png'), transparent: false, side: THREE.DoubleSide }), //bottom side
+    new THREE.MeshPhongMaterial({ map: loader.load('images/side.png'), transparent: false, side: THREE.DoubleSide }), //front side
+    new THREE.MeshPhongMaterial({ map: loader.load('images/side.png'), transparent: false, side: THREE.DoubleSide }), //back side
+];
+
+
+
+
+const cube = new THREE.Mesh( geometry, cubeMaterials );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+const light = new THREE.AmbientLight( 0xcccccc ); // soft white light
+scene.add( light );
+scene.add( directionalLight );
+scene.add( cube );
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+camera.position.z = 2;
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	cube.rotation.x = 0;
+	cube.rotation.y = 0;
+	cube.rotation.z = 0;
+	renderer.render( scene, camera );
+}
+var controls = new THREE.OrbitControls(camera,renderer.domElement);//创建控件对象
+controls.addEventListener('change', render);//监听鼠标、键盘事件
+animate();
+
