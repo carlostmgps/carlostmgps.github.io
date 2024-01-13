7a62d429dd3ded767a10c22dd76b1a8689492668
carlostmgps <61446775+carlostmgps@users.noreply.github.com>
2024-01-13 15:21:32 +0800

    Update main.js

--- ai/draw/main.js
+++ ai/draw/main.js
@@ -43,9 +43,9 @@ function reset(){
     container.innerHTML = ''
     populate(size)
 }
-setInterval({
-  reset();
-},1)
+setInterval(
+  reset
+,1)
 function updatedata(){
     datap.innerHTML = String(data);
 }
